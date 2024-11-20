17406137ee76c3dda87f5ea669e260de0965c387
Dnilson Achahuanco Huarilloclla <dnilson.ah.7@gmail.com>
2024-11-20 17:58:10 -0500

    Implementación de formas en el mapa

--- app/src/main/java/com/tecsup/lab12/MapScreen.kt
+++ app/src/main/java/com/tecsup/lab12/MapScreen.kt
@@ -5,15 +5,17 @@ import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Marker
 
 @Composable
 fun MapScreen() {
@@ -44,7 +46,6 @@ fun MapScreen() {
                 LatLng(-16.3524187,-71.5675994) // Zamacola
             )
 
-
             locations.forEach { location ->
                 Marker(
                     state = rememberMarkerState(position = location),
@@ -52,6 +53,73 @@ fun MapScreen() {
                     snippet = "Punto de interés"
                 )
             }
+
+            val mallAventuraPolygon = listOf(
+                LatLng(-16.432292, -71.509145),
+                LatLng(-16.432757, -71.509626),
+                LatLng(-16.433013, -71.509310),
+                LatLng(-16.432566, -71.508853)
+            )
+
+            val parqueLambramaniPolygon = listOf(
+                LatLng(-16.422704, -71.530830),
+                LatLng(-16.422920, -71.531340),
+                LatLng(-16.423264, -71.531110),
+                LatLng(-16.423050, -71.530600)
+            )
+
+            val plazaDeArmasPolygon = listOf(
+                LatLng(-16.398866, -71.536961),
+                LatLng(-16.398744, -71.536529),
+                LatLng(-16.399178, -71.536289),
+                LatLng(-16.399299, -71.536721)
+            )
+
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Black,
+                strokeWidth = 5f
+            )
+
+            // Dibuja un triángulo en Arequipa
+            Polygon(
+                points = listOf(
+                    LatLng(-16.398744, -71.54), // Punto B
+                    LatLng(-16.4449, -71.55), // Punto A
+                    LatLng(-16.4, -71.55), // Punto C
+                    LatLng(-16.398866, -71.54) // Volver al punto A para cerrar el triángulo
+                ),
+                strokeColor = Color.Red,
+                fillColor = Color.Magenta,
+                strokeWidth = 5f
+            )
+
+            // Dibuja un trapecio en Arequipa
+            Polygon(
+                points = listOf(
+                    LatLng(-16.398, -71.53), // Punto A
+                    LatLng(-16.3985, -71.5375), // Punto B
+                    LatLng(-16.3995, -71.5375), // Punto C
+                    LatLng(-16.400, -71.53), // Punto D
+                    LatLng(-16.398, -71.53) // Volver al punto A para cerrar el trapecio
+                ),
+                strokeColor = Color.Blue,
+                fillColor = Color.Cyan,
+                strokeWidth = 5f
+            )
         }
     }
 }
